66fc20f703b515c6eeace24d1970b5ea2d75981a
KevinFacanha <y0kevin@outlook.com>
2026-01-30 15:07:08 -0300

    adicionado método que salva dados usuário

--- src/main/java/com/kevin/usuario/infrastructure/entity/Endereco.java
+++ src/main/java/com/kevin/usuario/infrastructure/entity/Endereco.java
@@ -3,10 +3,7 @@ package com.kevin.usuario.infrastructure.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -14,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "endereco")
+@Builder
 public class Endereco {
 
     @Id
